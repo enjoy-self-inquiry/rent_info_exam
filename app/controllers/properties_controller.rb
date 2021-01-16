bd83c475eed51915f4dc18566328d2aa8e183ef9
masao takayama <m.takayama0929@gmail.com>
2021-01-16 07:08:58 +0000

    createアクションの実装

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,4 +5,11 @@ class PropertiesController < ApplicationController
   def new
     @property = Property.new
   end
+
+  def create
+    Property.create(name: params[:property][:name], price: params[:property][:price],
+                    address: params[:property][:address], age: params[:property][:age],
+                    remarks: params[:property][:remarks])
+    redirect_to properties_path
+  end
 end
